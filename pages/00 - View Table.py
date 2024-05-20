45c697589dba8f31267c3e7108720fc345fe5fa5
JCHU634 <87676426+jchu634@users.noreply.github.com>
2024-05-21 01:21:31 +1200

    Added page to view tables

---
+++ pages/00 - View Table.py
@@ -0,0 +1,24 @@
+import streamlit as st
+from data import util
+
+st.title("View a table")
+
+# We use a form to control when the page is (re)loaded and hence when the data is reset or retrieved.
+with st.form("form"):
+    table_name = st.selectbox("Table name", util.VALID_TABLE_NAMES)
+    submitted = st.form_submit_button("Run")
+
+if submitted:
+    assert table_name in util.VALID_TABLE_NAMES, "Invalid table name"
+
+    conn = util.get_connection()
+
+    result_df = conn.query(
+        f"select * from {table_name}",
+        ttl=0,  # don't cache results so changes in the database are immediately retrieved
+    )
+    st.dataframe(
+        result_df,
+        use_container_width=True,
+        hide_index=True,
+    )
