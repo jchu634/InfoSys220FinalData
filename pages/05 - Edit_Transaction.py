66c4f49d88676282bb66572c9d35bc9b70e204ac
JCHU634 <87676426+jchu634@users.noreply.github.com>
2024-05-28 20:33:34 +1200

    Patched Edit_transaction

--- pages/05 - Edit_Transaction.py
+++ pages/05 - Edit_Transaction.py
@@ -8,11 +8,7 @@ table_name = "fiscal_transaction"
 
 conn = util.get_connection()
 val_transaction_id = st.number_input("Transaction ID: ", step=1)
-
-val_new_date = st.date_input("New Transaction Date: ")
-val_set_new_date = st.checkbox("Set New Date")
 val_new_amount = st.number_input("New Transaction Amount: ", step=0.01)
-val_set_new_amount = st.checkbox("Set New Amount")
 
 # A query to get the current review details as a preview
 preview_results = conn.query(
@@ -31,20 +27,11 @@ with st.form("form"):
     submitted = st.form_submit_button("Edit Transaction")
 
 if submitted:
-    if not val_set_new_date and not val_set_new_amount:
-        st.write("Please select at least one field to update.")
-        st.stop()
-    if val_set_new_date:
-        conn._instance.execute(
-            f"UPDATE fiscal_transaction SET transaction_date = :val_new_date WHERE id = :val_transaction_id",
-            dict(val_new_date=val_new_date, val_transaction_id=val_transaction_id)
-        )
-    if val_set_new_amount:
-        conn._instance.execute(
-            f"UPDATE fiscal_transaction SET amount = :val_new_amount WHERE id = :val_transaction_id",
-            dict(val_new_amount=val_new_amount,
-                 val_transaction_id=val_transaction_id)
-        )
+    conn._instance.execute(
+        f"UPDATE fiscal_transaction SET amount = :val_new_amount WHERE id = :val_transaction_id",
+        dict(val_new_amount=val_new_amount,
+             val_transaction_id=val_transaction_id)
+    )
     results_df = conn.query(
         'SELECT * FROM fiscal_transaction WHERE id = :val_search_id',
         params=dict(val_search_id=val_transaction_id),
